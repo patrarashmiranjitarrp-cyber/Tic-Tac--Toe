1f772b4092c6a16a79624a2008dde9e399f74ce9
Rashmi <Rashmi@2006>
2026-02-21 21:28:32 +0530

    RP

---
+++ app.js
@@ -0,0 +1,30 @@
+let boxes=document.querySelectorAll(".box");
+let resetBtn=document.querySelector("#reset-btn");
+let turnO=true; //player x or player O
+ const winsPattern=[[0,1,2],[3,4,5],[6,7,8],[0,3,6],[1,4,7],[2,5,8],[0,4,8],[2,4,6]];
+ boxes.forEach((box)=>{
+    
+    box.addEventListener("click",()=>{
+        if(turnO){
+            box.innerText="O";
+            turnO=false;
+        }else{
+            box.innerText="X";
+            turnO=true;
+        }
+        box.disabled=true;
+        checkWinner();
+    })
+ })
+ const checkWinner=()=>{
+    for(let pattern of winsPattern){
+        let pos1val=boxes[pattern[0]].innerText;
+        let pos2val=boxes[pattern[1]].innerText;
+        let pos3val=boxes[pattern[2]].innerText;
+        if(pos1val!="" && pos2val!="" && pos3val!=""){
+            if(pos1val===pos2val && pos2val===pos3val){
+                console.log("winner");
+            }
+        }
+    }
+ }
